161d671f0a660cdb1e2ea537a614e565c69d0291
rfrecer <robert.frecer@gisat.cz>
2025-12-26 21:07:45 +0100

    Update vite.config.js
    Add base path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // This must match your repository name exactly
   base: '/terez-frecerova/',
 })

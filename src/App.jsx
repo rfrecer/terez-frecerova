e81c9cdd8592b1c0080dcae2530cf01a51e9d3dc
rfrecer <robert.frecer@gisat.cz>
2026-04-02 22:06:02 +0200

    Add concerts

--- src/App.jsx
+++ src/App.jsx
@@ -82,6 +82,9 @@ const App = () => {
   // Raw Concert Data
   // Add 'photoUrl' for past concerts to appear in the archive grid
   const rawConcerts = [
+    { venue: "Medze Festival", city: "Dolný Kubín, SK", date: "2026-09-05", link: "https://www.instagram.com/medzefestival/", imgColor: "bg-lime-200", photoUrl: "" },
+    { venue: "[TBA]", city: "Bratislava, SK", date: "2026-05-23", link: "", imgColor: "bg-fuchsia-200", photoUrl: "" },
+    { venue: "10 rokov Kurníka", city: "Bratislava, SK", date: "2026-05-21", link: "https://naskurnik.sk/", imgColor: "bg-orange-200", photoUrl: "" },
     { venue: "Literatura žije!", city: "České Budějovice, CZ", date: "2026-04-25", link: "https://www.literatura-zije.cz/", imgColor: "bg-cyan-200", photoUrl: "" },
     { venue: "United Islands (Klubová noc)", city: "Praha, CZ", date: "2026-04-30", link: "https://www.unitedislands.cz/cs", imgColor: "bg-rose-200", photoUrl: "" },
     { venue: "Žižkovská noc", city: "Praha, CZ", date: "2026-03-20", link: "https://www.facebook.com/events/865544609627654", imgColor: "bg-orange-200", photoUrl: "2026-03-20 zizkovska noc.jpg" },

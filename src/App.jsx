7c4918f8ff8937fea2562f8ebce78ce5c284ed7e
rfrecer <r.frecer@gmail.com>
2026-04-28 15:57:52 +0200

    literatura zije

--- src/App.jsx
+++ src/App.jsx
@@ -85,7 +85,7 @@ const App = () => {
     { venue: "Medze Festival", city: "Dolný Kubín, SK", date: "2026-09-05", link: "https://www.instagram.com/medzefestival/", imgColor: "bg-lime-200", photoUrl: "" },
     { venue: "[TBA]", city: "Bratislava, SK", date: "2026-05-23", link: "", imgColor: "bg-fuchsia-200", photoUrl: "" },
     { venue: "10 rokov Kurníka", city: "Bratislava, SK", date: "2026-05-21", link: "https://naskurnik.sk/", imgColor: "bg-orange-200", photoUrl: "" },
-    { venue: "Literatura žije!", city: "České Budějovice, CZ", date: "2026-04-25", link: "https://www.literatura-zije.cz/", imgColor: "bg-cyan-200", photoUrl: "" },
+    { venue: "Literatura žije!", city: "České Budějovice, CZ", date: "2026-04-25", link: "https://www.literatura-zije.cz/", imgColor: "bg-cyan-200", photoUrl: "2026-04-25 literatura zije.jpg" },
     { venue: "United Islands (Klubová noc)", city: "Praha, CZ", date: "2026-04-30", link: "https://www.unitedislands.cz/cs", imgColor: "bg-rose-200", photoUrl: "" },
     { venue: "Žižkovská noc", city: "Praha, CZ", date: "2026-03-20", link: "https://www.facebook.com/events/865544609627654", imgColor: "bg-orange-200", photoUrl: "2026-03-20 zizkovska noc.jpg" },
     { venue: "+ Tante Elze: Tiny Flájská", city: "Praha, CZ", date: "2026-01-24", link: "https://www.youtube.com/@TinyFl%C3%A1jsk%C3%A1", imgColor: "bg-teal-200", photoUrl: "2026-01-24 tiny flajska.jpg" },
